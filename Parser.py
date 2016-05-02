a4af709f1c90023543bf2d78d0ebc5f61806ee13
Darkstar000 <ian.shi@mail.utoronto.ca>
2016-05-02 14:15:33 -0400

    Added Parser, which allows the parsing of a data file to create a dict with JASPAR names and AGI targets

---
+++ Parser.py
@@ -0,0 +1,63 @@
+__author__ = 'Ruian'
+
+import re
+
+class Parser:
+    """
+    The parser module parses text files with to retrieve JASPAR names and their
+    targets which are in an AGI format. Information is stored in a dictionary,
+    with the JASPAR names as keys and AGI targets as values.
+
+    Attributes:
+        text_file (File): The file which is opened for further reading and
+            parsing of information
+        interaction_dictionary (Dict): The dictionary of interactions with
+            JASPAR names as keys, and AGI targets as values.
+    """
+
+    def __init__(self, file):
+        """(Parser, file) -> None
+
+        Initializes the parser module by opening the file.
+
+        Args:
+            text_file: A text file, with JASPAR names and AGI targets
+        """
+        # Open file for reading
+        self.text_file = open(file, "r")
+        # Initialize interaction dictionary
+        self.interaction_dictionary = {}
+        # Parser data file
+        self.parse_data_set()
+        # Close reading file
+        self.text_file.close()
+
+    def parse_data_set(self):
+        """(Parser) -> None
+
+        Parses the data file to obtain a dictionary of JASPAR names and AGI
+        targets.
+        """
+
+        # Regex to find JASPAR and AGI
+        regex = re.compile("(MA\d\d\d\d\.1).(AT\dG\d\d\d\d\d)")
+        # Reads all lines
+        for line in self.text_file:
+            # Find JASPAR and AGI target in current line
+            result = regex.match(line)
+
+            # Get JASPAR and AGI if match found
+            name_jas = ""
+            name_agi = ""
+
+            if result:
+                name_jas = result.group(1)
+                name_agi = result.group(2)
+
+            # Check if JASPAR name key exists in dictionary
+            if name_jas in self.interaction_dictionary:
+                # Appends agi target to list
+                self.interaction_dictionary[name_jas].append(name_agi)
+            else:
+                # Creates a new list of agi targets
+                self.interaction_dictionary[name_jas] = [name_agi]
